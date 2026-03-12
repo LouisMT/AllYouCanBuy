316c42416fa1cb6bd3998be35596a1f00a6bdff1
Louis Matthijssen <louis@u5r.nl>
2026-03-12 21:47:52 +0100

    Add helper to get appliance IDs

---
+++ AllYouCanBuy/LogAppliances.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Kitchen;
+using KitchenData;
+using KitchenMods;
+using UnityEngine;
+
+namespace AllYouCanBuy
+{
+    public class LogAppliances : GenericSystemBase, IModSystem
+    {
+        private bool _done;
+
+        protected override void OnUpdate()
+        {
+            if (_done)
+            {
+                return;
+            }
+
+            var appliances = GameData.Main.Get<Appliance>();
+
+            foreach (var appliance in appliances.Where(a => !string.IsNullOrWhiteSpace(a.Name)))
+            {
+                Debug.Log($"Name: {appliance.Name}, ID: {appliance.ID}");
+            }
+
+            _done = true;
+        }
+    }
+}
